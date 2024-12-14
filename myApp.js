ad84ec378bdbd3fae776edd5479afa86e311d741
mouradaitelouhab <mouradprotech@gmail.com>
2024-12-13 19:22:13 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -56,7 +56,8 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+const PORT = process.env.PORT || 3300;
+app.listen( PORT,() => {
+  console.log(`Mourad Information Security Project ${PORT}`)
+
+} );
